309f40fa16e40b7af5a2aad45a09973a7ec02661
Viriyadhika Putra <viriya.dhika0@gmail.com>
2020-08-10 10:47:59 +0800

    minor db change

--- items/tests.py
+++ items/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.urls import reverse
+# import requests
+# from users.models import User
+# from .models import Item
+
+# class TestItemLoggedIn(TestCase):
+    
+#     username = 'a_new_username'
+#     password = 'secure_password'
+
+#     def setUp(self):
+#         User.objects.create_user(
+#             username = self.username,
+#             password = self.password
+#         )
+#         self.client.post(reverse('login'), {'username': self.username, 'password': self.password})
+
+
+
+# class TestItemNotLoggedIn(TestCase):
+#     username = 'a_new_username'
+#     password = 'secure_password'
+
+#     def setUp(self):
+#         user = User.objects.create_user(
+#             username = self.username,
+#             password = self.password
+#         )
+#         self.client.post(reverse('login'), {'username': self.username, 'password': self.password})
+#         Item.objects.create(name = 'umbrella', desc = 'item_desc', quantity = 3, , None)
+
+
+#     def test_item_search(self):
+#         response = self.client.get()
 
 # Create your tests here.
