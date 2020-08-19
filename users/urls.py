d9940e8a0712f0c1573d09c5d22e3f4f16a73071
Viriyadhika Putra <viriya.dhika0@gmail.com>
2020-08-19 15:05:40 +0800

    added user detail page

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, UserDetailView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user_detail')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
+from .models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CustomUserCreationForm
 
@@ -8,5 +10,10 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    context_object_name = 'user'
+    template_name = 'user_detail.html'
+    model = User
 
 # Create your views here.
